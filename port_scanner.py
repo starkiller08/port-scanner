a1f8ae8690b495d3f9612fbee05120627c3665b8
Sarim Khan <sarimkhan752@gmail.com>
2026-07-13 18:22:06 +1000

    Added a functionality to let the user know the scanner is working perfectly behind the scenes.

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,45 @@
 from socket import *
+import time
+import sys
 
-domain = input("Enter the host address to scan (can be IP address or domain name): ")
+def animate():
+	for i in range(0,10):
+		sys.stdout.write('\rloading |')
+		time.sleep(0.1)
+		sys.stdout.write('\rloading /')
+		time.sleep(0.1)
+		sys.stdout.write('\rloading ---')
+		time.sleep(0.1)
+		sys.stdout.write("\rloading \\")
+		time.sleep(0.1)
+		i += 1
+	sys.stdout.write('\rProgram Started.... Welcome to Stealthy, the go to scanner for ethical hackers!!\n\n')
+
+print('*' * 49)
+print('     Stealthy -- The Ultimate Port Scanner!!')
+print('*' * 49)
+print('Initializing Program....')
+time.sleep(0.7)
+animate()
+
+
+domain = input("Enter the host address to scan (IP address or domain name): ")
 p = input("Enter the range of ports to scan (ex. 1,100 ): ")
 
 port = p.split(',')
 port_start = int(port[0])
-port_end = int(port[1])
+port_end = int(port[1]) + 1
 
 
 for i in range(port_start,port_end):
 	skt = socket(family=AF_INET, type=SOCK_STREAM)
-	skt.settimeout(4.00)
+	skt.settimeout(10.00)
+	print(f'Scanning port {i} of {port[1]}')
 	result = skt.connect_ex((domain,i))
 
 	if result == 0:
 		print(f"port {i} is open.")
 
 	skt.close()
+
+print('\nStealthy has finished scanning. Have fun exploiting the shit out of those ports ;) :D :D')
